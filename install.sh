d24e185064c021d13c1644d898341181a3ce91c7
Rafael Romero Lopes <rafarlopes@hotmail.com>
2015-10-12 22:05:37 -0300

    fixed structure to use bash repo

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# check if git was installed
+command -v git >/dev/null 2>&1 || {
+	echo >&2 "I require git but it's not installed. Aborting...";
+	exit 0;
+}
+
+# dotfiles
+DOTFILES_REPO="https://github.com/rafarlopes/dotfiles.git"
+
+git clone $DOTFILES_REPO ~/.dotfiles
+
+cp ~/.dotfiles/files/gitconfig ~/.gitconfig
+cp ~/.dotfiles/files/gitignore ~/.gitignore
+cp ~/.dotfiles/files/bash_profile ~/.bash_profile
+
+rm -Rf ~/.dotfiles
+
+#bash
+BASH_REPO="https://github.com/rafarlopes/bash.git"
+
+git clone $BASH_REPO ~/.bash
+
+source ~/.bash_profile
